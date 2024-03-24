745d84d0daea71c487d333d6342f2713db4b1294
mtricanowicz <163475820+mtricanowicz@users.noreply.github.com>
2024-03-24 19:13:03 -0400

    Add Category drop down selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,5 @@ st.write("### (2) add a multi-select for Sub_Category *in the selected Category 
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
+
+st.selectbox(label="Category", options=df["Category"])
