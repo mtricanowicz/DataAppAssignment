17577e63323d77bbf3e3ad9adc7d6eeea0dff4ae
mtricanowicz <163475820+mtricanowicz@users.noreply.github.com>
2024-03-24 19:54:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,5 +36,19 @@ st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
 
-st.selectbox(label="Category", options=df["Category"].unique())
-st.multiselect(label="Subcategory", options=df["Sub_Category"].unique())
+select_options=df["Category"].unique()
+
+st.selectbox(label="Choose a Category", options=select_options)
+
+cat_subcat=df[["Category","Sub_Category"]].drop_duplicates().sort_values(by=["Category", "Sub_Category"]).reset_index(drop=True)
+subcats_furniture=cat_subcat["Sub_Category"].where(cat_subcat["Category"]=="Furniture").dropna()
+subcats_officesupplies=cat_subcat["Sub_Category"].where(cat_subcat["Category"]=="Office Supplies").dropna()
+subcats_technology=cat_subcat["Sub_Category"].where(cat_subcat["Category"]=="Technology").dropna()
+
+multiselect_options={
+    "Furniture":subcats_furniture,
+    "Office Supplies":subcats_officesupplies,
+    "Technology":subcats_technology
+    }
+st.multiselect(label="Choose a Subcategory", options=multiselect_options)
+
