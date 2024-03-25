b72a7d2bb1beb5650e0e9c7ef9bd141dafe67d01
mtricanowicz <163475820+mtricanowicz@users.noreply.github.com>
2024-03-24 21:02:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,10 @@ selected_sales_by_month=df.where(df["Sub_Category"].isin(subcategory)).filter(it
 st.line_chart(selected_sales_by_month, y="Sales")
 
 # (4): show three metrics for the selected items in (2): total sales, total profit, and overall profit margin (%)
-selected_total_sales=st.metric(label="Total sales for selected subcategories:", value=f"${df.where(df["Sub_Category"].isin(subcategory)).filter(items=['Sales']).dropna().sum()}")
-selected_total_profit=st.metric(label="Total profit for selected subcategories:", value=f"${df.where(df["Sub_Category"].isin(subcategory)).filter(items=['Profit']).dropna().sum()}")
+sales_calc=round(df.where(df["Sub_Category"].isin(subcategory)).filter(items=['Sales']).dropna().sum(),2)
+selected_total_sales=st.metric(label="Total sales for selected subcategories:", value=f"${sales_calc}")
+profit_calc=round(df.where(df["Sub_Category"].isin(subcategory)).filter(items=['Profit']).dropna().sum(),2)
+selected_total_profit=st.metric(label="Total profit for selected subcategories:", value=f"${profit_calc}")
 margin_calc=round(((df.where(df["Sub_Category"].isin(subcategory)).filter(items=['Profit']).dropna().sum()["Profit"])/(df.where(df["Sub_Category"].isin(subcategory)).filter(items=['Sales']).dropna().sum()["Sales"]))*100,2)
 selected_overall_margin=st.metric(label="Overall margin for selected subcategories:", value=f"{margin_calc}%")
 
